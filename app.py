744a852eaff26c10698b74f3e33d0e014f58dd70
Mbali Sithebe <2567890@students.wits.ac.za>
2025-10-18 16:33:34 +0200

    Flask script updates

--- app.py
+++ app.py
@@ -1,73 +1,17 @@
-from flask import Flask, render_template, jsonify, request
-from src.helper import download_hugging_face_embeddings
-from langchain_pinecone import PineconeVectorStore
-from langchain_openai import ChatOpenAI
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from dotenv import load_dotenv
-from src.prompt import *
-import os
-
+from flask import Flask, render_template, request
+from src.pipeline import ask_question
 
 app = Flask(__name__)
 
-load_dotenv()
-
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
-
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-
-embeddings = download_hugging_face_embeddings()
-
-
-index_name = "medical-chatbot" 
-# Embed each chunk and upsert the embeddings into your Pinecone index.
-docsearch = PineconeVectorStore.from_existing_index(
-    index_name=index_name,
-    embedding=embeddings
-)
-
-
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
-
-# Initialize ChatOpenAI with OpenRouter GPT-OSS-20B free model
-chatModel = ChatOpenAI(
-    model="openai/gpt-oss-20b:free",
-    openai_api_base="https://openrouter.ai/api/v1"
-)
-
-# Define the system + human prompt template
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
-
-
-question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-
-
-
 @app.route("/")
 def index():
-    return render_template('chat.html')
+    return render_template("chat.html")
 
-
-@app.route("/get", methods=["GET", "POST"])
+@app.route("/get", methods=["POST"])
 def chat():
     msg = request.form["msg"]
-    input = msg
-    print(input)
-    response = rag_chain.invoke({"input": msg})
-    print("Response : ", response["answer"])
-    return str(response["answer"])
+    answer = ask_question(msg)
+    return answer
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
